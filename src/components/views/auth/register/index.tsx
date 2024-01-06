8aa8ed7d24e628860ef4588d02e2b9ff6e5034aa
Yanda Agil <yandaagil@gmail.com>
2024-01-06 20:14:26 +0700

    feat: init firebase and sign up page

---
+++ src/components/views/auth/register/index.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { FormEvent, useState } from "react"
+
+const Register = () => {
+  const { push } = useRouter()
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError('')
+    setIsLoading(true)
+    const form = e.target as HTMLFormElement
+
+    const data = {
+      fullname: form.fullname.value,
+      email: form.email.value,
+      password: form.password.value,
+    }
+
+    const result = await fetch('/api/user/register', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (result.status === 200) {
+      form.reset()
+      setIsLoading(false)
+      push('/auth/login')
+    } else {
+      setIsLoading(false)
+      setError('Email already exists!')
+    }
+  }
+
+  return (
+    <div className="mx-auto w-96 space-y-7">
+      <h1 className="text-center font-bold text-2xl">Sign Up</h1>
+      {error &&
+        <div role="alert" className="alert alert-error">
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>{error}</span>
+          <button className="btn btn-sm btn-circle" onClick={() => setError('')}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+          </button>
+        </div>
+      }
+      <form onSubmit={handleSubmit} className="space-y-5">
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="fullname">Name</label>
+          <input type="text" name="fullname" id="fullname" className="input input-bordered w-full" />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" autoComplete="on" className="input input-bordered w-full" />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" className="input input-bordered w-full" />
+        </div>
+        {isLoading ? (
+          <button className="btn btn-neutral btn-block">
+            <span className="loading loading-spinner"></span>
+            Loading
+          </button>
+        ) : (
+          <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+        )}
+      </form>
+      <p className="text-center">Already have an account? <Link href='/auth/login' className="link link-primary">Sign In</Link></p>
+    </div>
+  )
+}
+
+export default Register
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import Register from "@/components/views/auth/register"
+
+const RegisterPage = () => {
+  return (
+    <main className="flex min-h-screen items-center">
+      <Register />
+    </main>
+  )
+}
+
+export default RegisterPage
